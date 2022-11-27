ca19fe722eb048fccab858890ba2a0e5e12d5bc6
duyconan11 <middsock123@gmail.com>
2022-11-28 06:53:55 +0700

    Add files via upload
    a

---
+++ config.js
@@ -0,0 +1,32 @@
+// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+var config = {
+    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    texts: [
+        "Hi Cậu!",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "Chúng ta quen nhau đã lâu rồi",  // 同上...
+        "Nhưng chưa gặp nhau",
+        "Món quà này tớ gửi tới cậu vì không biết cậu ở đâu và cũng nói tớ là IT",
+        "Chúc cậu một sinh thần vui vẻ",
+        "Luôn mạnh khoẻ và tìm thấy tình yêu đời mình",
+        "Luôn vui vẻ và mỉm cười cậu nhé",
+        "Vì tớ cảm nhận được cậu cười sẽ xinh lắm",
+        "Đừng khóc vì ai nữa nhé",
+        "Nếu buồn thì tớ luôn ở sau lắng nghe cậu",
+        "Nguyễn Duy (oOoSh1Su1oOo) (--__--\")",
+    ],
+    imgs: {
+        "心爱的小可爱": "./imgs/xiaokeai.png",
+        // "今天是你的生日": "./imgs/birthday.jpg",
+    },
+    desc: {
+        turn_on: "Bắt Đầu",
+        play: "Next",
+        bannar_coming: "Next",
+        balloons_flying: "Next",
+        cake_fadein: "Next",
+        light_candle: "Next",
+        wish_message: "Next",
+        story: "A MESSAGE FOR YOU",
+    }
+};
